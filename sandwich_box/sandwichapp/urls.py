d3741e25066ef46e6b232256f44f072f956adc44
sasianan19 <sasianan19@gmail.com>
2022-04-08 21:02:43 -0600

    Add randomization

--- sandwich_box/sandwichapp/urls.py
+++ sandwich_box/sandwichapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from sandwichapp.views import SandwichappView, IngredientsListView
+from sandwichapp.views import SandwichappView, IngredientsListView, SandwichGeneratorView
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', SandwichappView.as_view(), name='sandwich'),
     #sandwich/ingredients/<str:ingredient_type>
     path('ingredients/<str:ingredient_type>', IngredientsListView.as_view(), name='ingredients_list'),
+    path('random', SandwichGeneratorView.as_view(), name='sandwich_generator')
 ]
--- sandwich_box/sandwichapp/views.py
+++ sandwich_box/sandwichapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import Http404
 from django.views import View
+from django.http import Http404
+import random
 
 
 ingredients = {
@@ -31,3 +32,13 @@ class IngredientsListView(View):
             template_name = 'ingredients_list.html',
             context = {'ingredients': ingredients[ingredient_type], 'ingredient_type': ingredient_type}
         )
+
+class SandwichGeneratorView(View):
+    def get(self, request):
+        if request.method == 'GET':
+            selected_meat = random.choice(ingredients['meats'])
+            selected_cheese = random.choice(ingredients['cheeses'])
+            selected_topping = random.choice(ingredients['toppings'])
+
+            sandwich = f'{selected_meat} & {selected_cheese} & {selected_topping}'
+            return render(request, 'sandwich_generator.html', context = {'sandwich': sandwich})
