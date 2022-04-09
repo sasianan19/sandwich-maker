6683ce994568b695dc3b61aa7f408154a3dfa795
sasianan19 <sasianan19@gmail.com>
2022-04-08 20:24:39 -0600

    Add SandwichApp view and IngredientsList view

--- sandwich_box/sandwichapp/urls.py
+++ sandwich_box/sandwichapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from sandwichapp.views import SandwichappView
 
 
 urlpatterns = [
-    
+    #sandwich
+    path('', SandwichappView.as_view(), name='sandwich')
 ]
--- sandwich_box/sandwichapp/views.py
+++ sandwich_box/sandwichapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class SandwichappView(View):
+    def get(self, request):
+        if request.method == 'GET':
+            return render(
+                request = request, 
+                template_name ='home.html', 
+                context = {'ingredients': ingredients.keys()}
+                )
+
+class IngredientsListView(View):
+    def get(self, request, ingredient_type):
+        return render(
+            request = request,
+            template_name = 'ingredients_list.html',
+            context = {'ingredients': ingredients[ingredient_type], 'ingredient_type': ingredient_type}
+        )
